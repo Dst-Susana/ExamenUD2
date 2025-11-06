a3f5ee29a90d867ca689b24b1b6faf762af15ea0
Dst-Susana <ssalmar2509@g.educaand.es>
2025-11-06 11:27:55 +0100

    Add files via upload

---
+++ SumaEnteros.java
@@ -0,0 +1,25 @@
+package ExUD2;
+
+public class SumaEnteros {
+	
+	public static void main (String[] args) {
+		try {
+			int [] numeros = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+			int resultado = calcularSuma(numeros);
+			System.out.println("La suma total es: " + resultado);
+		 
+	}
+		catch (Exception e) {
+			
+		}
+		public static int calcularSuma(int[] valores) throws Exception {
+			int suma =0;
+			for (int i=0; i <= valores.lenght; i++) {
+				suma += valores[i];
+			}
+		
+			return suma;
+		
+	}
+
+}
